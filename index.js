524f5a25aa2178229c914c55313215ce76990713
lidiabejarano <bejaranolidia@yahoo.com>
2020-08-24 21:47:47 -0300

    agregue ejercitacion

---
+++ index.js
@@ -0,0 +1,8 @@
+const elementoVerde = document.querySelector(".verde")
+elementoVerde.classList.remove("verde")
+
+const elementoRojo = document.querySelector(".rojo")
+elementoRojo.classList.add(".rosa")
+
+const elementoRosa = document.querySelector(".rosa")
+elementoRosa.classList.toggle("rosa")
